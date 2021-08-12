510f261c0c8d9599d1d70780f32dd6cc84243dd4
SEFAZ-CE <SEFAZ-CE@192.168.15.58>
2021-08-11 23:32:49 -0300

    configuracoes iniciais

---
+++ src/main/java/com/loja/controller/PrincipalController.java
@@ -0,0 +1,15 @@
+package com.loja.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PrincipalController {
+	
+	@GetMapping("/administrativo")
+	public String acessarPrincipal() {
+		return "administrativo/home";
+	}
+
+}
+ 
